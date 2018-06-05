7b62e6e25309b9d818a7b31fa22f392aca593f27
Manuk Karapetyan <kmanuk@MacBook-Pro-5.local>
2018-06-05 15:18:12 +0300

    Sum of numerous arguments, Traffic light, Transform To Prime

---
+++ Sum of numerous arguments/sum_of_numerous_arguments.rb
@@ -0,0 +1,21 @@
+#  http://www.codewars.com/kata/sum-of-numerous-arguments/ruby
+#  Task :
+#  After calling the function findSum() with any number of non-negative integer arguments, it should return the sum of all those arguments. If no arguments are given, the function should return 0, if negative arguments are given, it should return -1.
+#
+#  eg
+#
+#  find_sum(1,2,3,4); # returns 10
+#  find_sum(1,-2);    # returns -1
+#  find_sum();        # returns 0 0
+#  Hint: research the arguments object on google or read Chapter 4 from Eloquent Javascript
+
+# best => 11 clever => 5
+def find_sum(*args)
+  return -1 if args.any?{|a| a < 0}
+  args.reduce(0, :+)
+end
+
+# best => 2 clever => 6
+def find_sum(*vals)
+  vals.any? { |i| i<0 } ? -1 : vals.reduce(0, :+)
+end
---
+++ Thinkful - Logic Drills: Traffic light/traffic_light.rb
@@ -0,0 +1,38 @@
+#  http://www.codewars.com/kata/thinkful-logic-drills-traffic-light
+#  Task:
+#
+#  Thinkful - Logic Drills: Traffic light
+#  You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+#
+#  Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+#
+#  For example, update_light('green') should return 'yellow'.
+#
+
+# best => 6 clever => 11
+def update_light(current)
+  {"green" => "yellow", "yellow" => "red", "red" => "green"}[current]
+end
+
+# best => 2
+def update_light(current)
+  if current == 'green'
+    'yellow'
+  elsif current == 'yellow'
+    'red'
+  elsif current == 'red'
+    'green'
+  end
+end
+
+# not for all ruby versions
+def update_light(current)
+  case current
+  when 'green'
+    'yellow'
+  when 'yellow'
+    'red'
+  when 'red'
+    'green'
+  end
+end
